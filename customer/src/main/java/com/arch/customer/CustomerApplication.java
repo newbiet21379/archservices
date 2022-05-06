a5b003802321797e2e4e7fd4345fe779616d26fd
pqtrung2 <pqtrung2@cmcglobal.vn>
2022-05-06 17:24:02 +0700

    Changes for k8s

--- customer/src/main/java/com/arch/customer/CustomerApplication.java
+++ customer/src/main/java/com/arch/customer/CustomerApplication.java
@@ -4,13 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
                 "com.arch.customer",
                 "com.arch.amqp",
         }
-)@EnableEurekaClient
+)
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
+@EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.arch.clients"
 )
